124853a9e0ef61f6dd054aac43f87cfa03bdaefc
freddie-cyber <89332820+freddie-cyber@users.noreply.github.com>
2022-05-12 12:08:39 +1200

    Add files via upload

---
+++ hithere.py
@@ -0,0 +1,47 @@
+print("hi there")
+print(":)")
+input("say hi:")
+print("here is a guessing game hint: between 10-16")
+answer = input("type in the answer:")
+
+if answer == "13":
+	print("correct")
+else:
+	print("incorrect")
+
+
+print("before we let you go answer this math question")
+
+answers = input("5+5=")
+
+if answers == "10":
+	print("correct")
+else:
+	print("incorrect")
+
+print("type in your age")
+input("age:")
+
+print("bye bye :)")
+
+print("haahhahahaah just joking")
+
+print("one last question")
+
+answeryt = input("5x5=")
+
+
+if answeryt == "25":
+	print("correct")
+else:
+	print("incorrect")
+	print("i will keep you here forever you got to complete challenges if you wannnnt to leave")
+	bear = input("who will win bear or anaconda")
+
+if bear == anaconda:
+	print("correct")
+else:
+	print("incorrect")
+
+	
+	
